533a5bdc8fa2175a1c073d35f477e1b9f4425c25
Maryam-Bagheri79 <m.bagheri7939@gmail.com>
2021-11-16 19:06:03 +0330

    done crud

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 let form = document.getElementById("form");
 let tbody = document.getElementById("tbody");
-
+let i = null;
 let data = [];
 
 
@@ -12,10 +12,13 @@ function render() {
 
     data.forEach((item, index) => {
 
+        if (i == null) {
+
+        }
         let newUser = `
         <tr>
-            <th scope="col" class="text-center"><i class ="fa fa-close" onclick = "remove(event)"><i></th>
-            <th scope="col" class="text-center"><i class ="fa fa-edit" onclick = "edit(event)"><i></th>
+            <th scope="col" class="text-center"><i class ="fa fa-close" onclick = "remove(${index})"><i></th>
+            <th scope="col" class="text-center"><i class ="fa fa-edit" onclick = "edit(${index})"><i></th>
             <th scope="col" class="text-center name">${item.name}</th>
             <th scope="col" class="text-center family">${item.family}</th>
             <th scope="col" class="text-center email">${item.email}</th>
@@ -32,18 +35,57 @@ function render() {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    nameValue = document.getElementById("name").value;
-    familyValue = document.getElementById("family").value;
-    emailValue = document.getElementById("email").value;
-    phoneValue = document.getElementById("phone").value;
+    if (i == null) {
+        nameValue = document.getElementById("name").value;
+        familyValue = document.getElementById("family").value;
+        emailValue = document.getElementById("email").value;
+        phoneValue = document.getElementById("phone").value;
 
-    let user = {
-        name: nameValue,
-        family: familyValue,
-        email: email,
-        phone: phone
+        let user = {
+            name: nameValue,
+            family: familyValue,
+            email: emailValue,
+            phone: phoneValue
+        }
+
+        data.push(user);
+        render();
+    } else {
+
+        nameValue = document.getElementById("name").value;
+        familyValue = document.getElementById("family").value;
+        emailValue = document.getElementById("email").value;
+        phoneValue = document.getElementById("phone").value;
+
+        let userUpdate = {
+            name: nameValue,
+            family: familyValue,
+            email: emailValue,
+            phone: phoneValue
+        }
+
+        data = data.map((item, index) => i != index ? item : userUpdate);
+
+        i = null;
+
+        render();
+
     }
 
-    data.push(user);
-    render();
+
 })
+
+function remove(index) {
+    data.splice(index, 1);
+    render();
+}
+
+function edit(index) {
+
+    document.getElementById("name").value = data[index].name;
+    document.getElementById("family").value = data[index].family;
+    document.getElementById("email").value = data[index].email;
+    document.getElementById("phone").value = data[index].phone;
+
+    i = index;
+}
